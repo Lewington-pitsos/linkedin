125e5dd1eabf463b948d058c371481edb7f3e570
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-02 19:04:10 +1100

    setup_tables is working

--- rubyscripts/archivist.rb
+++ rubyscripts/archivist.rb
@@ -5,11 +5,32 @@ class Archivist
   @@database_name = 'linkedin'
 
   def initialize
-    self.db = PG.connect
+    self.db = PG.connect({ dbname: @@database_name, user: 'postgres' })
   end
 
-  def setup
+  def setup_tables
+    self.db.exec(
+      <<~HEREDOC
 
+        CREATE TABLE people (
+          id serial,
+          first_name VARCHAR,
+          last_name VARCHAR,
+          current_job VARCHAR,
+          country VARCHAR,
+          further_location VARCHAR,
+          education VARCHAR,
+          employer_id INTEGER REFERENCES employer(id) ON DELETE CASCADE,
+          PRIMARY KEY(id)
+        );
+
+        CREATE TABLE employer (
+          id serial,
+          name VARCHAR,
+          PRIMARY KEY(id)
+        );
+
+      HEREDOC
   end
 
 end
